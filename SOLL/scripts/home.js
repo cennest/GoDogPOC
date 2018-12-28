19a01319656b5099bcbba3f09bc6287bda7faafa
Nilesh-Bhor <nilesh.bhor@cennest.com>
2018-12-28 11:11:00 +0530

    splash screen for video

--- SOLL/scripts/home.js
+++ SOLL/scripts/home.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
         sldpPlayer = SLDP.init({
             container: "sldp_player_wrapper",
             stream_url: "ws://104.248.182.51:8081/live/1",
+            splash_screen : '../images/GoDogVideoSplash.png',
             height: 520,
             width: 680,
             autoplay: false
